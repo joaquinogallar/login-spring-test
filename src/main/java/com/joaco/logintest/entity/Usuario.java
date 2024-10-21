0ed0dd19a966fc867e6a6e8f94888803b5537e07
Joaquin Ogallar <joaquinnogallar@gmail.com>
2024-10-21 13:28:14 -0300

    feat: entidad usuario

---
+++ src/main/java/com/joaco/logintest/entity/Usuario.java
@@ -0,0 +1,31 @@
+package com.joaco.logintest.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private UUID id;
+
+    private String nombre;
+    private String apellido;
+    private String email;
+    private String password;
+
+    public Usuario(String nombre, String apellido, String email, String password) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.email = email;
+        this.password = password;
+    }
+}
