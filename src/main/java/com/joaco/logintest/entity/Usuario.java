963061bc3c208da338a7470b73e0c0a7b932c9ce
Joaquin Ogallar <joaquinnogallar@gmail.com>
2024-10-22 20:13:55 -0300

    fix: tipo del id de Usuario, nombre de la base de datos en la uri

--- src/main/java/com/joaco/logintest/entity/Usuario.java
+++ src/main/java/com/joaco/logintest/entity/Usuario.java
@@ -15,7 +15,7 @@ import java.util.UUID;
 public class Usuario {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private UUID id;
+    private Long id;
 
     private String nombre;
     private String apellido;
